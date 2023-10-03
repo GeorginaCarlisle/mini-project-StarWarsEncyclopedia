2d080d7d5ee9f91187df03f3368a6c515d52e7fd
Georgina Carlisle <georgina.carlisle@gmail.com>
2023-10-03 21:33:48 +0100

    Add button and event listener to find out more about film

--- assets/js/films-script.js
+++ assets/js/films-script.js
@@ -93,11 +93,43 @@ function displayFilmData(data) {
         const filmData = `
             <div class="bg-slate-900 p-3 mt-10 mx-8 rounded sm:mx-16 md:mx-8">
                 <h2 class="text-center text-red-500 text-2xl font-bold mb-4">${film.title}</h2>
-                <p class="mb-4"><span class="font-bold mr-3">Episode:</span><span>${film.episode_id}</span></p>
+                <p class="mb-4"><span class="font-bold mr-3">Episode:</span><span>${film.episode_id}</span><span class="font-bold mr-3 ml-6">Released:</span><span>${film.release_date}</span></p>
                 <p class="mb-4"><span class="font-bold mr-3">Opening crawl:</span><span>${film.opening_crawl}</span></p>
-            </div>
-        `;
+                <div class="flex justify-items-end">
+                    <button id="btn-filmData-${i}" class="bg-amber-500 hover:bg-red-700 p-1 text-xl text-black rounded-md mx-auto">
+                        Find out more
+                    </button>
+                </div>
+            </div>`
         // Add film data into the DOM
         infoContainer.insertAdjacentHTML("beforeend", filmData);
+        // Add event listener
+        const buttonClick = document.getElementById(`btn-filmData-${i}`);
+        buttonClick.addEventListener('click', extraFilmInfo);
     }
 }
+
+/**
+ * Called by event listener attached to find out more button
+ * Passed event and from this id of button pressed located
+ * 
+ */
+function extraFilmInfo(event) {
+    console.log(event.srcElement.id);
+    const buttonClicked = event.srcElement.id;
+}
+
+
+
+
+
+ // build extra film data
+ const extraFilmData = `
+ <div id="filmData-modal-${i}">
+     <h2 class="text-center text-red-500 text-2xl font-bold mb-4">${film.title}</h2>
+     <p class="mb-4"><span class="font-bold mr-3">Episode:</span><span>${film.episode_id}</span></p>
+     <p class="mb-4"><span class="font-bold mr-3">Director:</span><span>${film.director}</span></p>
+     <p class="mb-4"><span class="font-bold mr-3">Producer:</span><span>${film.producer}</span></p>
+     <p class="mb-4"><span class="font-bold mr-3">Director:</span><span>${film.director}</span></p>
+ </div>
+`;
