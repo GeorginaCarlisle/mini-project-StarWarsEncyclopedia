7cef6bc223ca808669d6e8057b1b607c012ecb42
Georgina Carlisle <georgina.carlisle@gmail.com>
2023-10-03 13:44:27 +0100

    connect to film data of API

--- assets/js/films-script.js
+++ assets/js/films-script.js
@@ -2,10 +2,29 @@
 const API_URL = "https://ci-swapi.herokuapp.com/api/films/"
 
 // Load page before populating with the data from the API
-window.onload = loadingMessage();
+window.onload = getFilmData();
 
 /**
  * Called on load
+ * Connects to SWAPI passing data to ??? function
+ * Calls LoadingMessage while connecting
+ * Displays error message to user if connection fails
+ */
+async function getFilmData() {
+    loadingMessage();
+    const response = await fetch(API_URL);
+    const data = await response.json();
+
+    if (response.ok) {
+        displayFilmData(data);
+    } else {
+        displayErrorMessage(data);
+        throw new Error(data.error);
+    }
+}
+
+/**
+ * Called while connection being made to SWAPI
  * Displays a message to inform the user that the data is loading
  */
 function loadingMessage() {
@@ -17,7 +36,40 @@ function loadingMessage() {
     messageContainer.style = "display:block";
     // Add heading and message
     heading.innerHTML = "Have patience";
-    contents.innerHTML = "On its way the film information is"
+    contents.innerHTML = `
+    On its way the film information is<br><br>
+    However if waiting a long time you are, please try again or contact us
+    `
+}
+
+/**
+ * Called once API has been successfully contected too
+ */
+function displayFilmData(data) {
+    // Locate items in the DOM to be altered
+    messageContainer = document.getElementById("message-container");
+    heading = document.getElementById("message-heading");
+    contents = document.getElementById("message-contents");
+    // Display message container
+    messageContainer.style = "display:block";
+    // Add heading and message
+    heading.innerHTML = "Time to celebrate";
+    contents.innerHTML = "correctly connected to the API you are";
+}
+
+/**
+ * Called if error is received when connecting to API
+ */
+function displayErrorMessage(data) {
+        // Locate items in the DOM to be altered
+        messageContainer = document.getElementById("message-container");
+        heading = document.getElementById("message-heading");
+        contents = document.getElementById("message-contents");
+        // Display message container
+        messageContainer.style = "display:block";
+        // Add heading and message
+        heading.innerHTML = "More work to be done";
+        contents.innerHTML = "failed to connect to the API you have";
 }
 
 
