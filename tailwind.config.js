abd2c571e4b0218ce808ed197bfeeb52383b1fdb
Georgina Carlisle <georgina.carlisle@gmail.com>
2023-10-03 11:01:49 +0100

    Add about page viewable on mobile screens

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html", "./films.html"],
+  content: ["./index.html", "./films.html", "./contact.html", "./about.html"],
   theme: {
     extend: {},
   },
